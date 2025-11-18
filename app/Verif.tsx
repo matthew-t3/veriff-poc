433c1b0936afe4a2641794869624da13a5a66a0d
Matthew Torres <matthew.torres@terminal3.io>
2025-11-18 16:37:14 +0800

    Update veriff page

--- app/Verif.tsx
+++ app/Verif.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { createVeriffFrame } from "@veriff/incontext-sdk";
+import { createVeriffFrame, MESSAGES } from "@veriff/incontext-sdk";
 import { useEffect, useState } from "react";
 
 export function Verif() {
   const [sessionURL, setSessionURL] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFinished, setIsFinished] = useState(true);
 
   async function getSession() {
+    setIsLoading(true);
     const response = await fetch("/api/session");
 
     if (!response.ok) {
@@ -21,6 +24,7 @@ export function Verif() {
     }
 
     setSessionURL(data.verification.url);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -28,7 +32,7 @@ export function Verif() {
       return;
     }
 
-    const veriffFrame = createVeriffFrame({
+    createVeriffFrame({
       url: sessionURL,
       embedded: true,
       embeddedOptions: {
@@ -36,14 +40,45 @@ export function Verif() {
       },
       onEvent: (event) => {
         console.log("event", event);
+        if ([MESSAGES.CANCELED, MESSAGES.RELOAD_REQUEST].includes(event)) {
+          setSessionURL("");
+        }
+
+        if (event === MESSAGES.FINISHED) {
+          setIsFinished(true);
+        }
+      },
+      onReload: () => {
+        setSessionURL("");
       },
     });
   }, [sessionURL]);
 
+  if (isFinished) {
+    return (
+      <div className="flex h-svh w-full flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold">Verification finished</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="flex h-svh w-full flex-col">
-      <button onClick={getSession}>Get Session</button>
-      <div id="veriff-container" className="w-full h-full"></div>
+      {!sessionURL && (
+        <button
+          onClick={getSession}
+          className="bg-blue-200 p-2 rounded-md w-fit mx-auto cursor-pointer disabled:bg-blue-50 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading..." : "Get Session"}
+        </button>
+      )}
+      <div
+        id="veriff-container"
+        className="w-full h-full text-center justify-center flex items-center"
+      >
+        {!sessionURL && "Veriff will be loaded here"}
+      </div>
     </div>
   );
 }
